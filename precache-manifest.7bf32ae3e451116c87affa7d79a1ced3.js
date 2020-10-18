08a05b217a63a335e96b8ae2d1b885a30f4f52c1
Alex Xiang <xiangzhe0812@gmail.com>
2020-10-18 22:13:54 +1000

    Updates

---
+++ precache-manifest.7bf32ae3e451116c87affa7d79a1ced3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ab447f48a5445ba802b718533f2ada0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f8128ced78bde337213f",
+    "url": "/static/css/2.3cf5d70f.chunk.css"
+  },
+  {
+    "revision": "b331cd9e9767d50fe040",
+    "url": "/static/css/main.b290f423.chunk.css"
+  },
+  {
+    "revision": "f8128ced78bde337213f",
+    "url": "/static/js/2.9063be32.chunk.js"
+  },
+  {
+    "revision": "3f8541e7044114d64fa56f2801eb7c4c",
+    "url": "/static/js/2.9063be32.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b331cd9e9767d50fe040",
+    "url": "/static/js/main.4aebd677.chunk.js"
+  },
+  {
+    "revision": "e9af31ee3cc93dcb7d95",
+    "url": "/static/js/runtime-main.fe791e74.js"
+  }
+]);
